46d98e1cda8aa7bc18cf0b6b9c0ce4d9a24199c7
umut <umut_baykan@hotmail.com>
2023-05-22 12:44:20 +0100

    preparing test data using the fixtures

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,9 +1,23 @@
 const {Shop, Item} = require("../src/gilded_rose");
+const { requirements } = require("./texttest_fixture");
+const TestItems = require("./texttest_fixture").items
+const TestRequirements = require("./texttest_fixture").requirements
 
-describe("Gilded Rose", function() {
-  it("should foo", function() {
+describe("Gilded Rose", () => {
+
+  let shop;
+  let items;
+  let requirements;
+
+  beforeEach(() => {
+    items = TestItems
+    requirements = TestRequirements
+    shop = new Shop(items)
+  })
+
+  it("should foo", () => {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    expect(items[0].name).toBe("foo");
   });
 });
--- js-jest/test/texttest_fixture.js
+++ js-jest/test/texttest_fixture.js
@@ -1,4 +1,3 @@
-
 const { Shop, Item } = require("../src/gilded_rose");
 
 const items = [
@@ -8,20 +7,71 @@ const items = [
   new Item("Sulfuras, Hand of Ragnaros", 0, 80),
   new Item("Sulfuras, Hand of Ragnaros", -1, 80),
   new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 10, 10),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 5, 10),
 
   // This Conjured item does not work properly yet
   new Item("Conjured Mana Cake", 3, 6),
 ];
 
-const days = Number(process.argv[2]) || 2;
-const gildedRose = new Shop(items);
+// Content commented out to ensure data does not print in tests
 
-console.log("OMGHAI!");
-for (let day = 0; day < days; day++) {
-  console.log(`\n-------- day ${day} --------`);
-  console.log("name, sellIn, quality");
-  items.forEach(item => console.log(`${item.name}, ${item.sellIn}, ${item.quality}`));
-  gildedRose.updateQuality();
+// const days = Number(process.argv[2]) || 2;
+// const gildedRose = new Shop(items);
+// console.log("OMGHAI!");
+
+// for (let day = 0; day < days; day++) {
+//   console.log(`\n-------- day ${day} --------`);
+//   console.log("name, sellIn, quality");
+//   items.forEach(item => console.log(`${item.name}, ${item.sellIn}, ${item.quality}`));
+//   gildedRose.updateQuality();
+// }
+
+const requirements = {
+  day1: [
+    ["+5 Dexterity Vest", 9, 19],
+    ["Aged Brie", 1, 1],
+    ["Elixir of the Mongoose", 4, 6],
+    ["Sulfuras, Hand of Ragnaros", 0, 80],
+    ["Sulfuras, Hand of Ragnaros", -1, 80],
+    ["Backstage passes to a TAFKAL80ETC concert", 14, 21],
+    ["Backstage passes to a TAFKAL80ETC concert", 9, 12],
+    ["Backstage passes to a TAFKAL80ETC concert", 4, 13],
+    ["Conjured Mana Cake", 2, 4]
+  ],
+  day2: [
+    ["+5 Dexterity Vest", 8, 18],
+    ["Aged Brie", 0, 2],
+    ["Elixir of the Mongoose", 3, 5],
+    ["Sulfuras, Hand of Ragnaros", 0, 80],
+    ["Sulfuras, Hand of Ragnaros", -1, 80],
+    ["Backstage passes to a TAFKAL80ETC concert", 13, 22],
+    ["Backstage passes to a TAFKAL80ETC concert", 8, 14],
+    ["Backstage passes to a TAFKAL80ETC concert", 3, 16],
+    ["Conjured Mana Cake", 1, 2]
+  ],
+  day3: [
+    ["+5 Dexterity Vest", 7, 17],
+    ["Aged Brie", -1, 4],
+    ["Elixir of the Mongoose", 2, 4],
+    ["Sulfuras, Hand of Ragnaros", 0, 80],
+    ["Sulfuras, Hand of Ragnaros", -1, 80],
+    ["Backstage passes to a TAFKAL80ETC concert", 12, 23],
+    ["Backstage passes to a TAFKAL80ETC concert", 7, 16],
+    ["Backstage passes to a TAFKAL80ETC concert", 2, 19],
+    ["Conjured Mana Cake", 0, 0]
+  ],
+  day6: [
+    ["+5 Dexterity Vest", 4, 14],
+    ["Aged Brie", -4, 10],
+    ["Elixir of the Mongoose", -1, 0],
+    ["Sulfuras, Hand of Ragnaros", 0, 80],
+    ["Sulfuras, Hand of Ragnaros", -1, 80],
+    ["Backstage passes to a TAFKAL80ETC concert", 9, 27],
+    ["Backstage passes to a TAFKAL80ETC concert", 4, 23],
+    ["Backstage passes to a TAFKAL80ETC concert", -1, 0],
+    ["Conjured Mana Cake", -3, 0]
+  ],
 }
+
+module.exports = {items: items, requirements: requirements}
